f969db1fa983b3c72159862d163d2cc109e75b2c
Amanda Morris <amandafmorris@gmail.com>
2017-01-21 19:21:59 +0000

    Added confirmation page, but job not rendering correctly

--- server.py
+++ server.py
@@ -25,8 +25,28 @@ def app_form():
                  "software_architect": "Software Architect",
                  "product_manager": "Product Manager"
                  }
+    # job = "software_engineer"
+    # print open_jobs[job]
 
     return render_template("/application-form.html", open_jobs=open_jobs)
+
+
+@app.route("/application-success", methods=["POST"])
+def app_form_confirmation():
+    """Return a confirmation of a submitted job application"""
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    #todo: add handling for non-numerical inputs for salary
+    salary = float(request.form.get("salary"))
+    job = request.form.get("job")
+    # print job
+    # print type(job)
+    return render_template("/application-response.html",
+                           first_name=first_name,
+                           last_name=last_name,
+                           salary=salary,
+                           job=job
+                           )
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
